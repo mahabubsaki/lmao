28d457afb96342e5559316230f4430197447ebbf
mahabubsaki <md929820@gmail.com>
2026-04-30 21:36:29 +0600

    fix: correct link text for React documentation

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
             <li>
               <a href="https://react.dev/" target="_blank">
                 <img className="button-icon" src={reactLogo} alt="" />
-                Learn more22
+                Learn more222
               </a>
             </li>
           </ul>
