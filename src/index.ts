88a5afb89be7561a84a5922539d7351b119d1cd4
mahabubsaki <md929820@gmail.com>
2026-05-01 01:30:34 +0600

    sssds

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 7000;
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({ message: "Hello from Express + TypeSc2rip2t2" });
+  res.json({ message: "Hello from Express + Type2Sc2rip2t2" });
 });
 
 app.get("/health", (_req, res) => {
