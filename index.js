ca94ab3fac89341e5ae7c5c1a0af8ca4d04bb500
Mahabub Saki <96987600+mahabubsaki@users.noreply.github.com>
2026-05-01 07:47:13 +0000

    ne

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const fastify = require('fastify')({ logger: true })
-fastify.get('/', async () => ({ hello: 'fastify' }))
+fastify.get('/', async () => ({ hello: 'fastifys' }))
 fastify.listen({ port: 3000, host: '0.0.0.0' })
