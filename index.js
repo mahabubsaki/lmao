cf96ee4ef0a86397246dae82fd9a06c63498ef5e
Mahabub Saki <96987600+mahabubsaki@users.noreply.github.com>
2026-05-01 11:03:54 +0000

    fix typo in server response message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Server is Running!</h1>');
+  res.send('<h1>Expsress Server is Running!</h1>');
 });
 
 app.listen(3000, () => {
