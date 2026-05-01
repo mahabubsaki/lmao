807e414e8ec1604a6bbb0a2375044f32e5fee679
Mahabub Saki <96987600+mahabubsaki@users.noreply.github.com>
2026-05-01 12:46:47 +0600

    ddsa

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 7000;
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({ message: "SESXH2e2ll2o2 s frodm Exp2ress + SDSD" + process.env.LOL });
+  res.json({ message: "SESXH2e2ll2o2 s 2 frodm Exp2ress + SDSD" + process.env.LOL });
 });
 
 app.get("/healths", (_req, res) => {
