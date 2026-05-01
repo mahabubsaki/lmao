dff5079666c3942acb2a4595daf035158b2b2535
Mahabub Saki <96987600+mahabubsaki@users.noreply.github.com>
2026-05-01 07:54:27 +0000

    🚀 Test: NestJS Enterprise Backend

--- index.js
+++
@@ -1,8 +0,0 @@
-const { serve } = require('@hono/node-server')
-const { Hono } = require('hono')
-const app = new Hono()
-app.get('/', (c) => c.json({ message: 'Hello Hono + Node.js!' }))
-serve({
-  fetch: app.fetch,
-  port: 3000
-})
