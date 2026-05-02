ff81847897c9d577700d10e394354df20f1c8fb5
Mahabub Saki <96987600+mahabubsaki@users.noreply.github.com>
2026-05-02 22:13:09 +0600

    sdsd

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(1001)
+  const [count, setCount] = useState(10011)
 
   return (
     <>
